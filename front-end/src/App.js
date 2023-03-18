5c5c7944a15ac92a998393ce1160ed9a3931ae83
a-j-hwang <alicia.jimin.h@gmail.com>
2023-03-18 19:11:03 -0400

    Config deleted from app.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,29 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
-import Cart from './Cart'
-
-const config = {
-  cartItems: [
-    {
-      id: 1,
-      name: "Cheeseburger",
-      price: 8.99,
-      quantity: 2,
-    },
-    {
-      id: 2,
-      name: "Fries",
-      price: 3.99,
-      quantity: 1,
-    },
-  ],
-  deliveryFee: 2.99,
-  taxRate: 0.1,
-};
+import Home from './Home'
 
 function App() {
   return (
-      <Cart config={config} />
+      <Home />
   );
 }
 
