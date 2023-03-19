07a792ad3dec26a23a13fd7dfd67d90f07cae5cb
Willcky <3076023745@qq.com>
2023-03-18 22:04:54 -0400

    add axios to fetch from mock api

--- front-end/src/components/reviewCards/reviewCard.jsx
+++ front-end/src/components/reviewCards/reviewCard.jsx
@@ -83,7 +83,7 @@ function ReviewCard(props) {
                 <SimpleImageSlider
                     width={dimensions.width}
                     height={200}
-                    images={["https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+                    images={["https://picsum.photos/200/300",
                     "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80",
                     "https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",]}
                     showBullets={true}
