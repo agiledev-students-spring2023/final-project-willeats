ba00ceeb23fa4a963055a2ab64d77ef05efe7106
a-j-hwang <alicia.jimin.h@gmail.com>
2023-03-19 22:26:36 -0400

    Trying to figure out orderfinished (checkout) page

--- front-end/src/Cart.js
+++ front-end/src/Cart.js
@@ -3,29 +3,6 @@ import './bootstrap.css'
 import './cart.css'
 
 
-/*
-Structue of config 
-
-const config = {
-  cartItems: [
-    {
-      id: 1,
-      name: "Cheeseburger",
-      price: 8.99,
-      quantity: 2,
-    },
-    {
-      id: 2,
-      name: "Fries",
-      price: 3.99,
-      quantity: 1,
-    },
-  ],
-  deliveryFee: 2.99,
-  taxRate: 0.1,
-};
-*/
-
 const Cart = ({ config }) => {
   const cartItems = config.cartItems;
   const taxRate = config.taxRate;
@@ -46,9 +23,12 @@ const Cart = ({ config }) => {
 
   return (
     <div className="cart-overlay">
+      /* Closing button below
+        Navigation (routing) will be implemented later */
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
-  <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
-</svg>
+        <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+      </svg>
+      
       <div className="cart">
         <h5>Your Cart</h5>
         {cartItems.map((item) => (
