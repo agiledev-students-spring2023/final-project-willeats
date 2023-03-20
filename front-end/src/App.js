e76dd7c55697f79268627b8518930f412ca8187a
a-j-hwang <alicia.jimin.h@gmail.com>
2023-03-19 22:27:41 -0400

    OrderFinished page created

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,31 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
-import Home from './Home'
+import Cart from './Cart'
+import OrderFinished from './OrderFinished'
+
+const config = {
+  cartItems: [
+    {
+      id: 1,
+      name: "Cheeseburger",
+      price: 8.99,
+      quantity: 2,
+    },
+    {
+      id: 2,
+      name: "Fries",
+      price: 3.99,
+      quantity: 1,
+    },
+  ],
+  deliveryFee: 2.99,
+  taxRate: 0.1,
+};
 
 function App() {
   return (
-      <Home />
+      <Cart config={config} />
+      
   );
 }
 
