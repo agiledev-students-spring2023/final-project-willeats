c16d65390a0c79ceddb0840e7ddb8eee2142ccce
Willcky <3076023745@qq.com>
2023-04-08 23:45:06 -0400

    update multer

--- back-end/app.js
+++ back-end/app.js
@@ -21,6 +21,7 @@ app.use(express.json()) // decode JSON-formatted incoming POST data
 app.use(express.urlencoded({ extended: true }))
 app.use("/static", express.static("public"))
 
+const upload = multer({dest: "./public/uploads" })
 
 app.get('/userpastreview', (req, resp) => {
     axios.get('https://my.api.mockaroo.com/pastreview1234.json?key=3c15f680')
@@ -50,19 +51,11 @@ app.post('/edituserreview', (req, resp) => {
 });
 
 
-app.post('/createuserreview', (req, resp) => {
-    console.log(req.body.saveData)
+app.post('/createuserreview', upload.array("image", 9), (req, resp) => {
+    console.log(req.files)
+    console.log(req.body)
     resp.status(200).send({message: 'create successfully'})
 });
-
-
-
-
-
-
-
-
-
 
 
 module.exports = app
