e4a2bfc5b090cf4e8b906ede358944245e8bfb7f
HYS20010113 <sherlockli0222@163.com>
2023-04-21 22:08:12 -0400

    update schema in db

--- back-end/db.js
+++ back-end/db.js
@@ -1,4 +1,7 @@
-import mongoose, { Schema } from 'mongoose';
+// import mongoose, { Schema } from 'mongoose';
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
 
 const UserSchema = new Schema({
     _id: Schema.Types.ObjectId,
@@ -9,10 +12,17 @@ const UserSchema = new Schema({
     review : [{type: Schema.Types.ObjectId, ref: 'Review'}],
     date: { type: Date, default: Date.now },
     avater: {type: Buffer},
-
 });
 
-const reviewSchema = new Schema({
+const RestaurantSchema = new Schema({
+    _id: Schema.Types.ObjectId,
+    name : {type: String, unique: true, required: true},
+    username : {type: String, unique: true, required: true},
+    email:{type: String, unique: true, required: true},
+    avatar:{type: Buffer, required: false},    
+})
+
+const ReviewSchema = new Schema({
     _id: Schema.Types.ObjectId,
     itemName: {type: String, required: true},
     dishId: { type: Schema.Types.ObjectId, ref: 'Dish' },
@@ -23,8 +33,39 @@ const reviewSchema = new Schema({
     image:[{type: Schema.Types.ObjectId, ref: 'ReviewImage'}]
 })
 
+const DishSchema = new Schema({
+    _id: Schema.Types.ObjectId,
+    restaurant :{
+        type:Schema.Types.ObjectId,
+        ref: 'restaurant'
+    },
+    photo: {
+      type: Buffer,
+      required: false
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true
+    },
+    review: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Review'
+    }]
+  });
 
+  const Restaurant = mongoose.model('Restaurant',RestaurantSchema);
+  const Dish = mongoose.model('Dish',DishSchema);
+  const User = mongoose.model('User', UserSchema);
+  const Review = mongoose.model('Review', ReviewSchema);
+  mongoose.connect("mongodb+srv://ljr:123123123@cluster0.tmtz8nv.mongodb.net/?retryWrites=true&w=majority",{
+    useNewUrlParser: true,
+});
 
+module.exports = { User, Review, Dish, Restaurant };
 
 
 
