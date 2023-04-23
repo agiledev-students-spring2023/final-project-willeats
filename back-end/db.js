d5520b5fa9ac02e576e8ab0e5fe95f2ede35a24b
Willcky <3076023745@qq.com>
2023-04-23 05:06:13 -0400

    update

--- back-end/db.js
+++ back-end/db.js
@@ -1,17 +1,31 @@
-import mongoose, { Schema } from 'mongoose';
+// import mongoose, { Schema } from 'mongoose';
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
 
 const UserSchema = new Schema({
-    _id: Schema.Types.ObjectId,
-    username : {type: String, unique: true, required: true},
+    // _id: Schema.Types.ObjectId,
+    name : {type: String, unique: true, required: true},
     email : {type: String, unique: true, required: [true, "Please provide a email!"]},
     password : {type: String, unique: true, required: [true, "Please provide a password!"]},
     order : [{ type: Schema.Types.ObjectId, ref: 'Order' }],
     review : [{type: Schema.Types.ObjectId, ref: 'Review'}],
     date: { type: Date, default: Date.now },
     avater: {type: Buffer},
-
 });
 
+<<<<<<< HEAD
+=======
+const RestaurantSchema = new Schema({
+    // _id: Schema.Types.ObjectId,
+    name : {type: String, unique: true, required: true},
+    email:{type: String, unique: true, required: true},
+    password : {type: String, unique: true, required: [true, "Please provide a password!"]},
+    createdate: { type: Date, default: Date.now },
+    avatar:{type: Buffer},    
+})
+
+>>>>>>> 4e15b87cf2f3f690d2d748937bb86fcc8262d19f
 const ReviewSchema = new Schema({
     _id: Schema.Types.ObjectId,
     itemName: {type: String, required: true},
@@ -23,6 +37,7 @@ const ReviewSchema = new Schema({
     image:[{type: Schema.Types.ObjectId, ref: 'ReviewImage'}]
 })
 
+<<<<<<< HEAD
 const RestaurantSchema = new Schema({
     // _id: Schema.Types.ObjectId,
     name : {type: String, unique: true, required: true},
@@ -31,6 +46,8 @@ const RestaurantSchema = new Schema({
     createdate: { type: Date, default: Date.now },
     avatar:{type: Buffer},    
 })
+=======
+>>>>>>> 4e15b87cf2f3f690d2d748937bb86fcc8262d19f
 const DishSchema = new Schema({
     _id: Schema.Types.ObjectId,
     restaurant :{
@@ -64,9 +81,12 @@ const DishSchema = new Schema({
 });
 
 module.exports = { User, Review, Dish, Restaurant };
+<<<<<<< HEAD
 
 
 
+=======
+>>>>>>> 4e15b87cf2f3f690d2d748937bb86fcc8262d19f
 
 
 
