6328fab169e85b61a52b8816906a68d453366a16
jhjoseph2 <jiangjh02@gmail.com>
2023-04-30 14:45:44 -0400

    update checkout and home page

--- front-end/src/components/Home/Home.js
+++ front-end/src/components/Home/Home.js
@@ -1,23 +1,27 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate,useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import './Home.css'
 import '../../bootstrap.css'
 import logo from "../../image/circle.png";
 import backgroundImage from "../../image/food-image.jpg";
-import mockProfileImg from "../../image/random-profile.jpg";
-import defaultProfileImg from "../../image/default-profile-image.svg";
+import ProfileButton from "../ProfileButton/ProfileButton"
 
 
 function Home() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  // const [restaurantId,setRestaurantId] = useState( useParams() )
-  const profileImage = isLoggedIn ? mockProfileImg : defaultProfileImg;
   const Navigate = useNavigate();
-  const {restId} = useParams();
+  const [token, setToken] = useState(localStorage.getItem("token"))
+
+  const { restId } = useParams();
   localStorage.setItem('restId', restId)
 
-  const navigateLogin = () => {
-    Navigate('/Login');
+  const handleLog = () => {
+    if (token) {
+      setToken(null);
+      localStorage.removeItem("token");
+      window.location.reload();
+    } else {
+      Navigate('/Login');
+    }
   }
 
   const navigateMenu = () => {
@@ -26,29 +30,31 @@ function Home() {
   }
 
   useEffect(() => {
-    
+
   }, []);
 
   return (
-    <div className="container"> 
-        <div className="background-container">
-            <img src={backgroundImage} alt="Background" className="background-image"/>
+    <div className="container">
+      <div className="background-container">
+        <img src={backgroundImage} alt="Background" className="background-image" />
+      </div>
+      <div className="content-container">
+        <div className="logo-container">
+          <img src={logo} alt="Logo" className="logo" />
         </div>
-        <div className="content-container">
-            <div className="logo-container">
-                <img src={logo} alt="Logo" className="logo" />
-            </div>
         <div className="profile-container">
-          <img src={profileImage} alt="Profile" className="profile" onClick={navigateLogin}/>
+          <ProfileButton />
         </div>
-      
+
         <div className="button-container">
-            <button className="button btn btn-primary btn-lg" onClick={navigateMenu}>Enter Menu</button>
+          <button className="button btn btn-primary btn-lg" onClick={navigateMenu}>Enter Menu</button>
         </div>
         <div className="login-container">
-          <button className="btn btn-link" onClick={navigateLogin}>Login</button>
+          <button className="btn btn-link" onClick={handleLog}>
+            {token ? "Logout" : "Login"}
+          </button>
         </div>
-        </div>
+      </div>
     </div>
   );
 }
