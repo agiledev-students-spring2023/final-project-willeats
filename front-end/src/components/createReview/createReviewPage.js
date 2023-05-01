3df518e9e193fd3a1b401dfd7cd7deeb1cdf6fa1
HYS20010113 <sherlockli0222@163.com>
2023-04-30 21:01:54 -0400

    update create Reveiw page

--- front-end/src/components/createReview/createReviewPage.js
+++ front-end/src/components/createReview/createReviewPage.js
@@ -4,7 +4,8 @@ import '../../bootstrap.css'
 import ReviewArea from '../reviewArea/reviewArea';
 import './createReviewPage.css'
 import PageBackButton from '../pagebackButton/PageBackButton'; 
-import axios from '../axiosConfig'
+// import axios from '../axiosConfig'
+import axios from 'axios';
 
 function CreateReviewPage(){
     const navigate = useNavigate()
@@ -35,7 +36,7 @@ function CreateReviewPage(){
                 saveData[i].image.forEach((e) => {
                     formData.append("image", e)
                 })
-                const newRequest = axios.post("/createuserreview", formData, {
+                const newRequest = axios.post("http://localhost:3001/createuserreview", formData, {
                     headers: {Authorization: `Bearer ${token}`}  
                 })
                 request.push(newRequest)
