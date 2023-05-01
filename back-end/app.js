49cb7f2fc05a15cb63e0d3d25771eff67b6aa57e
HYS20010113 <sherlockli0222@163.com>
2023-04-30 22:10:01 -0400

    update the order place

--- back-end/app.js
+++ back-end/app.js
@@ -381,7 +381,7 @@ app.post('/checkout/:restId', (req, res) => {
         order
             .save()
             .then(result => {
-                res.status(200).send({ message: 'checkout successfully' })
+                res.status(200).send(result)
             })
             .catch(err => {
                 console.log(err)
